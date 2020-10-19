203b57e8b9084fb074e7cc32c0dd07b1b1f1e955
Amine Zeghmi <amin@MacBook-Pro-de-Amine.local>
2020-10-19 16:47:17 +0200

    modified Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -33,7 +33,7 @@ class Header extends Component {
 
       <div className="row banner">
          <div className="banner-text">
-            <h1 className="responsive-headline">{name}.</h1>
+            <h1 className="responsive-headline">{name}</h1>
             <h3>Je suis <span>{occupation}</span> à {city}. {description}</h3>
             <hr />
             <ul className="social">
